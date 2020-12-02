6d1bc5d9158785d9804202d378dd22c22585a034
Alexander Kacharava <a.kacharava94@gmail.com>
2020-12-02 15:46:54 +0100

    Merged projects & tweaking on prefab object

---
+++ Assets/Scripts/AutoPlaceObjects.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class AutoPlaceObjects : MonoBehaviour
+{
+    ///ARRaycastManager raycastManager;
+    [SerializeField]
+    ARPlaneManager arPlaneManager;
+
+    [SerializeField]
+    GameObject spawnedObject;
+
+    private void Awake()
+    {
+        arPlaneManager = GetComponent<ARPlaneManager>();
+        arPlaneManager.planesChanged += PlaneUpdate;
+    }
+
+    private void PlaneUpdate(ARPlanesChangedEventArgs args)
+    {
+        if (args.added != null)
+        {
+            ARPlane arPlane = args.added[0];
+            Instantiate(spawnedObject, arPlane.transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/RandomObjectSpaw.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjectSpaw : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spawnObject;
+
+    float minLookDistance = 1f;
+    float maxLookDistance = 3.5f;
+    int spawnCount = 0;
+
+    ///Vector3 
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 7, 7);
+    }
+
+    void SpawnObject()
+    {
+        if (spawnCount < 3)
+        {
+            Instantiate(spawnObject, this.transform.position, Quaternion.identity);
+            spawnCount++;
+        }
+    }
+}
